48e5e08eb40ed48adb6650537e25b598f4b64d30
Như Ý <97014485+ysneef@users.noreply.github.com>
2026-03-14 23:00:58 +0700

    Update MyConstants.js

--- apps/api/utils/MyConstants.js
+++ apps/api/utils/MyConstants.js
@@ -1,14 +1,14 @@
 const MyConstants = {
-  DB_SERVER: 'cluster0.xko2qpo.mongodb.net',
-  DB_USER: 'vohuynhnhuy',
-  DB_PASS: 'vohuynhnhuy',
-  DB_DATABASE: 'shoppingonline',
-  JWT_SECRET: 'vohuynhnhuy',
-  JWT_EXPIRES: '10h',
-  EMAIL_USER: 'vohuynhnhuy30@gmail.com',
-  EMAIL_PASS: 'atcv zumo trfb xgzi'
+  DB_SERVER: process.env.DB_SERVER,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_DATABASE: process.env.DB_DATABASE,
+
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES: process.env.JWT_EXPIRES,
+
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS
 };
 
-export default MyConstants
-
-//mongodb+srv://vohuynhnhuy:vohuynhnhuy@cluster0.xko2qpo.mongodb.net/
+export default MyConstants;
